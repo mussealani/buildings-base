1998ff8dbdddcd5a1f5687ff0927bf7d8a8fedfa
Jens <jcc@jenschrchristensen.com>
2016-03-29 14:24:11 +0200

    Sorting out duplicate directories and files starting with '.'

--- index.php
+++ index.php
@@ -38,12 +38,16 @@
         $handle = opendir($directory);
         if ($handle) {
             while (false !== ($file = readdir($handle))) {
-            preg_match("/(^(([\.]){1,2})$|(\.(svn|git|md))|(Thumbs\.db|\.DS_STORE))$/iu", $file, $skip);
-            if($exclude){
-                preg_match($exclude, $file, $skipByExclude);
-            }
-            if (!$skip && !$skipByExclude) {
+              if (strpos($file, '.') === 0){
+                  continue;
+              }
+              preg_match("/(^(([\.]){1,2})$|(\.(svn|git|md))|(Thumbs\.db|\.DS_STORE))$/iu", $file, $skip);
+              if($exclude){
+                  preg_match($exclude, $file, $skipByExclude);
+              }
+              if (!$skip && !$skipByExclude) {
                 if (is_dir($directory. DIRECTORY_SEPARATOR . $file)) {
+                    
                     if($recursive) {
                         $arrayItems = array_merge($arrayItems, directoryToArray($directory. DIRECTORY_SEPARATOR . $file, $recursive, $listDirs, $listFiles, $exclude));
                     }
@@ -51,12 +55,12 @@
                         $file = $directory . DIRECTORY_SEPARATOR . $file;
                         $arrayItems[] = $file;
                     }
-                } else {
-                    if($listFiles){
-                        $file = $directory . DIRECTORY_SEPARATOR . $file;
-                        $arrayItems[] = $file;
-                    }
-                }
+              } else {
+                  if($listFiles){
+                      $file = $directory . DIRECTORY_SEPARATOR . $file;
+                      $arrayItems[] = $file;
+                  }
+              }
             }
         }
         closedir($handle);
